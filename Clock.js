e783166a743aab5041f15bf1154d197c88c9336e
Dheeraj2907 <dheerajsharma.cbse@gmail.com>
2025-04-05 16:07:47 +0530

    changes

--- Clock.js
+++ Clock.js
@@ -1,4 +1,14 @@
 let clock = document.getElementById("clock");
+let countOfHour = 12;
+//Angle Difference in hour
+let AngleDiff = 360 / countOfHour;
+let angle = AngleDiff + 45;
+//loop the the count of hour for the UI
+for (let i = 1; i <= countOfHour; i++) {
+  clock.innerHTML += `<div class="fs-1 fw-bold position-absolute " style="transform: rotate(${angle}deg); height:290px;width:290px;"><span class="position-absolute text-center" style="transform: rotate(${-angle}deg);height:50px;width:50px;" >${i}</span></div>`;
+  angle += AngleDiff;
+}
+//Setting the current time
 function setCurrentTime() {
   let currentTime = new Date();
   let miliSecond = currentTime.getMilliseconds();
@@ -6,15 +16,16 @@ function setCurrentTime() {
   let Minutes = currentTime.getMinutes() + Second / 60;
   let Hours = currentTime.getHours() + Minutes / 60;
   document.getElementById("SecondNeddle").style.transform = `rotate(${
-    Second * 6 -90
+    Second * 6 - 90
   }deg)`;
   document.getElementById("MinutesNeedle").style.transform = `rotate(${
-    Minutes * 6 -90
+    Minutes * 6 - 90
   }deg)`;
   document.getElementById("HourNeedle").style.transform = `rotate(${
-    Hours * 30 -90
+    Hours * AngleDiff - 90
   }deg)`;
 }
+//Set Intervel for clock
 window.onload = function (e) {
   let clockinterval = window.setInterval(function () {
     setCurrentTime();
